a53a4315113487713ac52be257e6a53f2074c040
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-29 20:22:26 -0400

    switched to polygon.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-const API_KEY = 'buc39mf48v6oa2u4eqvg';
+const API_KEY = 'fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z';
+
+
+
+let makeCard = () => {
+  let response = fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
+    console.log(response)
+  });
+}
+makeCard('AAPL')
