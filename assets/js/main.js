290c3943ef4991a1641a005b45db59707fbd8985
jcaswell <jakecaswell88@gmail.com>
2021-09-29 20:24:11 -0400

    added google charts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
 const API_KEY = 'buc39mf48v6oa2u4eqvg';
+var chartData = "https://finnhub.io/api/v1/stock/candle?symbol=" + userSearch + "&resolution=D&from=1572651390&to=1575243390&token=buc39mf48v6oa2u4eqvg"
+// needed for google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
