2f00101275570ba47aa3a97ef21e1d5695faf9ca
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-29 21:01:59 -0400

    gets data from polygon.io api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,31 @@
 const API_KEY = 'fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z';
+let cardContainerEl = $('#card-container');
 
 
 
-let makeCard = () => {
-  let response = fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
-    console.log(response)
+
+
+
+let getBasicData = (ticker) => {
+  //format yesterdays date because api does not have current day data available
+  let yesterdaysDayOfYear = (moment().dayOfYear()) - 1;
+  let date = moment(yesterdaysDayOfYear).format('YYYY-MM-DD')
+  console.log(date)
+  let response = fetch('https://api.polygon.io/v1/open-close/' + ticker+ '/' + date + '?adjusted=true&apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
+    response.json().then((data) => {
+      console.log(data)
+    })
+  })
+  
+}
+let makeCard = (ticker) => {
+  let response = fetch('https://api.polygon.io/v1/meta/symbols/' + ticker + '/company?apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
+    response.json().then((data) => {
+      console.log(data)
+      let cardEl = $('<div>').addClass("row bg-light text-dark p-4").html('<h3 class="h3">' + ticker + '</h3><p class="h5 text-secondary">' + data.name + '</p>');
+      cardContainerEl.append(cardEl);
+      getBasicData(ticker);
+    })
   });
 }
 makeCard('AAPL')
