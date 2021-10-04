eff84051eab12477044ed00ffae0594f8189daff
jcaswell <jakecaswell88@gmail.com>
2021-10-04 19:35:58 -0400

    fixed chart function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const API_KEY = 'fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z';
+const API_KEY = 'sandbox_bvhn01v48v6olk04psp0';
 var searchInputEl = document.getElementById("search-input");
 var searchButtonEl = document.getElementById("search-button");
 // needed for google charts
@@ -15,7 +15,7 @@ let watchListTickers = [];
 // }
 
 var getChartData = function(stockTicker) {
-    fetch("https://api.polygon.io/v1/open-close/" + stockTicker + "/2021-09-28?adjusted=true&apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z")
+    fetch("https://finnhub.io/api/v1/stock/candle?symbol=" + stockTicker + "&resolution=D&from=1631022248&to=1631627048&token=sandbox_bvhn01v48v6olk04psp0")
         .then(response => response.json())
         .then(data => drawChart(data));
     
@@ -24,11 +24,11 @@ var getChartData = function(stockTicker) {
 // function to make the chart
 var drawChart = function(data) {
     var data = google.visualization.arrayToDataTable([
-        [data.symbol, data.open, data.low, data.high, data.close],
-        ['Tue', 31, 38, 55, 66],
-        ['Wed', 50, 55, 77, 80],
-        ['Thu', 77, 77, 66, 50],
-        ['Fri', 68, 66, 22, 15]
+        ['Mon', data.l[0], data.o[0], data.c[0], data.h[0]],
+        ['Tue', data.l[1], data.o[1], data.c[1], data.h[1]],
+        ['Wed', data.l[2], data.o[2], data.c[2], data.h[2]],
+        ['Thu', data.l[3], data.o[3], data.c[3], data.h[3]],
+        ['Fri', data.l[4], data.o[4], data.c[4], data.h[4]]
     ], true);
 
     var options = {
