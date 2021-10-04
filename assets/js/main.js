5c2926794cf4005389c6700ea109adfca371755d
jcaswell <jakecaswell88@gmail.com>
2021-10-04 16:17:42 -0400

    chart function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 const API_KEY = 'fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z';
-
 // needed for google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+//google.charts.load('current', {'packages':['corechart']});
+//google.charts.setOnLoadCallback(makeChart);
 
 
-let makeCard = () => {
-  let response = fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
-    console.log(response)
-  });
+// let makeCard = () => {
+//   let response = fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z').then((response) => {
+//     console.log(response)
+//   });
+// }
+
+var getChartData = function() {
+    fetch("https://api.polygon.io/v1/open-close/AAPL/2021-09-28?adjusted=true&apiKey=fKzrzAwj3Su_nDWNEMyaa6lLvXCrFX8z")
+        .then(response => response.json())
+        .then(data => console.log(data));
+    
 }
-makeCard('AAPL')
+
+//makeCard('AAPL')
+getChartData();
