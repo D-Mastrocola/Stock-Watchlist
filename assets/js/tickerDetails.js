9f127949e85d14b53fd2f9b08217592e58eab782
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-10-05 09:00:28 -0400

    added jakes charts to details page

--- assets/js/tickerDetails.js
+++ assets/js/tickerDetails.js
@@ -1,4 +1,7 @@
 const API_KEY = "sandbox_bvhn01v48v6olk04psp0";
+// needed for google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
 
 let queryString = document.location.search;
@@ -7,11 +10,35 @@ console.log(selectedTicker);
 
 let tickerDetailsEl = $('#ticker-details').addClass('row').html("<h2 class'col-12'>" + selectedTicker + '</h2>');
 
+let getChartData = function(stockTicker) {
+  fetch("https://finnhub.io/api/v1/stock/candle?symbol=" + stockTicker + "&resolution=D&from=1631022248&to=1631627048&token=sandbox_bvhn01v48v6olk04psp0")
+      .then(response => response.json())
+      .then(data => drawChart(data));
+  
+}
+
+// function to make the chart
+var drawChart = function(data) {
+  var data = google.visualization.arrayToDataTable([
+      ['Mon', data.l[0], data.o[0], data.c[0], data.h[0]],
+      ['Tue', data.l[1], data.o[1], data.c[1], data.h[1]],
+      ['Wed', data.l[2], data.o[2], data.c[2], data.h[2]],
+      ['Thu', data.l[3], data.o[3], data.c[3], data.h[3]],
+      ['Fri', data.l[4], data.o[4], data.c[4], data.h[4]]
+  ], true);
+
+  var options = {
+      legend:'none'
+    };
+  var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+}
 
 let getTickerDetails = async (ticker) => {
   let tradeInfoEl = $('<div>').addClass("col-3 row");
   let companyInfoEl = $('<div>').addClass('col-3 row');
-  let graph = $('<img>').addClass('col-6').attr('src', './assets/images/image place holder.jpg');
+  let graph = $('<div>').addClass('col-6').attr('id', 'chart_div');
   let quoteData = await fetch("https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + API_KEY).then((response) => {
     response.json().then((data) => {
       console.log(data)
@@ -31,7 +58,7 @@ let getTickerDetails = async (ticker) => {
       let yearRange = $('<div>').addClass('col-12 row p-2').html("<span class='col-6 text-start'>52 Week Range: </span><span class='col-6 text-end fw-bold'>" + data.metric['52WeekLow'].toFixed(2) + " - " + data.metric['52WeekHigh'].toFixed(2) + "</span>");
       tradeInfoEl.append(yearRange)
 
-      let marketCapText = data.metric.marketCapitalization;
+      let marketCapText = data.metric.marketCapitalization.toFixed(0);
       //Market cap is 10 ^ -6
       if(data.metric.marketCapitalization / 1_000_000 > 1) {
         marketCapText = (data.metric.marketCapitalization/1_000_000).toFixed(2) + "M";
@@ -49,8 +76,8 @@ let getTickerDetails = async (ticker) => {
       companyInfoEl.append(marketCap)
     })
   });
-
-
-  tickerDetailsEl.append(tradeInfoEl, companyInfoEl, graph)
+  tickerDetailsEl.append(tradeInfoEl, companyInfoEl, graph);
+  getChartData(ticker);
 }
+
 getTickerDetails(selectedTicker);
